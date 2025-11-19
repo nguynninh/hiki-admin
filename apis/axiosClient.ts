1b0a314179b6c33e7626461af67989eb5e987011
Nguyen Ninh <nguyenvanninh2004pt@gmail.com>
2025-11-19 22:47:56 +0700

    feat: implement authentication API with axios client and handle API requests

---
+++ apis/axiosClient.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import queryString from "query-string";
+
+const axiosClient = axios.create({
+  baseURL:
+    process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
+      ? process.env.NEXT_PUBLIC_PRODUCTION_URL
+      : process.env.NEXT_PUBLIC_BASE_URL,
+  paramsSerializer: (params) => queryString.stringify(params),
+});
+
+axiosClient.interceptors.request.use(async (config: any) => {
+  config.headers = {
+    Authorization: '',
+    Accept: "application/json",
+    ...config.headers,
+  };
+
+  return { ...config, data: config.data ?? null };
+});
+
+axiosClient.interceptors.response.use(
+  (res) => {
+    if (res.data && res.status >= 200 && res.status < 300) {
+      return res.data;
+    } else {
+      return Promise.reject(res.data);
+    }
+  },
+  (error) => {
+    const { response } = error;
+    return Promise.reject(response.data);
+  }
+);
+
+export default axiosClient;
