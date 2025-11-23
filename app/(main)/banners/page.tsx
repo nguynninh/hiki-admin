fc13586e0b92a5251192a2775d02e667eb14154a
Nguyen Ninh <nguyenvanninh2004pt@gmail.com>
2025-11-23 17:49:23 +0700

    feat: Add new banners page with placeholder content.

---
+++ app/(main)/banners/page.tsx
@@ -0,0 +1,19 @@
+const UsersPage = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+                fontSize: "2rem",
+                fontWeight: "bold",
+                fontFamily: "sans-serif",
+            }}
+        >
+            Danh sách banner
+        </div>
+    );
+};
+
+export default UsersPage;
